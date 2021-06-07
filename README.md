e4649d3e365b63c082d961ae9503891f815657a0
Chris Klanac <1008777+cklanac@users.noreply.github.com>
2021-06-07 10:32:00 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Meals_app
+
+Meals App from https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/
---
+++ android/app/src/main/kotlin/com/chicagoworks/flutter_complete_guide_meals_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.chicagoworks.meals_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
